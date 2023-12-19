88057c9c4109dc683afba4049f9a5a7b17a11978
heyyou <hey3you3@gmail.com>
2023-12-20 01:13:59 +0900

    feat: load toml config file

--- adapter/input/cli/main.go
+++ adapter/input/cli/main.go
@@ -5,27 +5,45 @@ import (
 	"alb-log-json/adapter/output/filestorage"
 	"alb-log-json/port/fetch_alb_log"
 	"context"
-	"github.com/aws/aws-sdk-go-v2/config"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"os"
 )
 
-func run() int {
+type config struct {
+	AWSProfileName string `toml:"aws_profile_name"`
+	BucketName     string `toml:"bucket_name"`
+	S3Key          string `toml:"s3_key"`
+}
+
+func loadConfig() (*config, error) {
+	var conf config
+	_, err := toml.DecodeFile("./alblogjson-config.toml", &conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
+
+func run(conf *config) int {
 	ctx := context.Background()
-	cfg, _ := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile(""))
-	bucketName := ""
+	cfg, _ := awsconfig.LoadDefaultConfig(ctx, awsconfig.WithSharedConfigProfile(conf.AWSProfileName))
 
 	fileStorageAdapter := &filestorage.OutputFileStorageS3Adapter{
 		S3Client: s3.NewFromConfig(cfg),
-		Bucket:   &bucketName,
+		Bucket:   &conf.BucketName,
 	}
 	input := fetch_alb_log.FetchALBLogInput{Usecase: &fetch_alb_log.FetchALBLogUsecase{FetchALBLogParam: filestorage.FetchALBLogParam{
 		Ctx:      ctx,
-		FilePath: "",
+		FilePath: conf.S3Key,
 	}, Output: &fetch_alb_log.FetchALBLogOutput{FileStorageAdapter: fileStorageAdapter}}}
 	albLogs, err := input.Invoke()
 
 	if err != nil {
+		fmt.Printf("Error: %s\n", err)
 		return 1
 	}
 
@@ -35,5 +53,10 @@ func run() int {
 }
 
 func main() {
-	os.Exit(run())
+	conf, err := loadConfig()
+	if err != nil {
+		fmt.Printf("Error: failed load toml config.\nCreate `alblogjson-config.toml`.\n")
+		os.Exit(1)
+	}
+	os.Exit(run(conf))
 }
