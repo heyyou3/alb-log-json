e5f3a953d77744e33185a4b668476ed533908853
heyyou <hey3you3@gmail.com>
2023-12-19 21:45:28 +0900

    feat: create port fetch_alb_log

--- port/fetch_alb_log/input.go
+++ port/fetch_alb_log/input.go
@@ -1,1 +1,14 @@
 package fetch_alb_log
+
+type IFetchALBLogInput interface {
+	Invoke() ([]*FetchALBLogStruct, error)
+}
+
+type FetchALBLogInput struct {
+	Usecase IFetchALBLogUsecase
+}
+
+func (i *FetchALBLogInput) Invoke() ([]*FetchALBLogStruct, error) {
+	// TODO: user input value validation
+	return i.Usecase.Invoke()
+}
